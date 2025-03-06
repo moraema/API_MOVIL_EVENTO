5f4f178c5b156fa7c2d6b324d047605674c959a3
emma <223729@ids.upchiapas.edu.mx>
2025-03-05 23:48:33 -0600

    correcion

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminControllers = require('../controllers/admin.controllers');
 const middlewareController = require('../middlewares/auth.middleware');
 
-router.post('/', middlewareController.verificarJwt, adminControllers.create);
+router.post('/',  adminControllers.create);
 router.delete('/:id', middlewareController.verificarJwt, adminControllers.deleteAdministrador);
 
 module.exports = router;
